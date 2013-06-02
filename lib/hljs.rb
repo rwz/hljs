157a165d81f7e9bf5affb9cb2f02237f3e1c9861
Pavel Pravosud <pavel@pravosud.com>
2013-06-02 19:42:54 +0700

    Implement switching adapters

--- lib/hljs.rb
+++ lib/hljs.rb
@@ -8,11 +8,24 @@ module HLJS
   def_delegators :adapter, :highlight, :supported_syntaxes
 
   def adapter
-    @adapter ||= begin
-      require "hljs/adapters/highlight_js"
-      Adapters::HighlightJS.instance
-    end
+    self.adapter = :highlightjs unless @adapter
+    @adapter
   end
 
-  module_function :adapter, :highlight, :supported_syntaxes
+  def adapter=(new_adapter)
+    @adapter = case new_adapter.to_s
+    when /highlight[\s_]?js/i
+      require "hljs/adapters/highlight_js"
+      Adapters::HighlightJS.instance
+    when /syntax[\s_]?highlighter/i
+      require "hljs/adapters/syntax_highlighter"
+      Adapters::SyntaxHighlighter.instance
+    else
+      raise ArgumentError, "invalid adapter #{new_adapter.inspect}"
+    end
+
+    @adapter
+  end
+
+  module_function :adapter, :adapter=, :highlight, :supported_syntaxes
 end
--- spec/hljs_spec.rb
+++ spec/hljs_spec.rb
@@ -1,12 +1,6 @@
 require "spec_helper"
 
 describe HLJS do
-  describe "#adapter" do
-    it "should be HighlightJS" do
-      expect(described_class.adapter).to be_kind_of(described_class::Adapters::HighlightJS)
-    end
-  end
-
   it "delegates #highlight to adapter" do
     expect(described_class.adapter).to receive(:highlight).with("foo", "bar")
     described_class.highlight "foo", "bar"
@@ -15,5 +9,31 @@ describe HLJS do
   it "delegates #supported_syntaxes to adapter" do
     expect(described_class.adapter).to receive(:supported_syntaxes).once
     described_class.supported_syntaxes
+  end
+
+  describe "#adapter=" do
+    args = ["highlightjs", "highlight_js", "highlight js", "HIGHLIGHTJS",
+        :highlightjs, :highlight_js]
+
+    args.each do |arg|
+      it "should enable HighlightJS with #{arg.inspect}" do
+        described_class.adapter = arg
+        expect(described_class.adapter).to be_instance_of(described_class::Adapters::HighlightJS)
+      end
+    end
+
+    args = ["SyntaxHighlighter", "Syntax Highlighter", "syntax_highlighter",
+      :syntax_highlighter, :syntaxhighlighter, :SyntaxHighlighter]
+
+    args.each do |arg|
+      it "should enable SyntaxHighlighter with #{arg.inspect}" do
+        described_class.adapter = arg
+        expect(described_class.adapter).to be_instance_of(described_class::Adapters::SyntaxHighlighter)
+      end
+    end
+
+    it "raises an ArgumentError on invalid input" do
+      expect{HLJS.adapter=:foo}.to raise_error(ArgumentError, "invalid adapter :foo")
+    end
   end
 end
